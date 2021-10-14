46b2aba12ffd17d9552c89399c5fe10eaa871022
Maxime Blais <blais.maxime@cegepvicto.ca>
2021-10-13 21:11:02 -0400

    Exemple de référence au Switch dans le ThirdFragment

--- app/src/main/java/com/example/footscore/FirstFragment.kt
+++ app/src/main/java/com/example/footscore/FirstFragment.kt
@@ -17,14 +17,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FirstFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/footscore/ThirdFragment.kt
+++ app/src/main/java/com/example/footscore/ThirdFragment.kt
@@ -1,6 +1,7 @@
 package com.example.footscore
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,27 +22,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ThirdFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
-    }
-
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false)
-
+        val view: View = inflater.inflate(R.layout.fragment_third, container, false)
+        val switch = view.findViewById<SwitchCompat>(R.id.mode)
+        switch.setOnCheckedChangeListener { _, isChecked -> Log.i("ValeurDuSwitch", isChecked.toString()) }
+        return view
     }
 
 
